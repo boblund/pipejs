be1e9646b8462f59772d54bbc3e4c0226e5202e4
Bob <blund@cablelabs.com>
2022-03-25 14:56:30 -0600

    First commit

---
+++ pipe-test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const {pipe, tap} = require('./pipe.js')
+
+const brumeParams = {
+  url: 'url',
+  token: 'token',
+  thisUser: 'thisUser',
+  baseDir: 'baseDir',
+  eventQueue: 'eventQueue',
+  networkEvents: 'networkEvents'
+};
+
+function createWebsocket({url, token, ...rest}) {
+  // url and token removed from pass through params
+  return {ws: 'ws', ...rest}  // ws added to pass through params
+}
+
+function makePeerConnection({...rest}) {
+  return {peerConnection: 'peerConnection', ...rest}
+}
+
+function BrumeData(parms) {
+  let {thisUser, baseDir, eventQueue, networkEvents} = parms // used but not removed
+  return {brumeData:'brumeData', ...parms}
+}
+
+function sender({peerConnection, eventQueue, brumeData}) {
+  console.log('sender:', peerConnection, eventQueue, brumeData)
+}
+
+function receiver({peerConnection, eventQueue, brumeData, networkEvents}) {
+  console.log('receiver:', peerConnection, eventQueue, brumeData, networkEvents)
+}
+
+// Cannot put tap(receiver) in pipe
+// i.e. pipe(..., tap(receiver), ...) won't work
+
+const tee = tap(receiver)
+pipe(createWebsocket,makePeerConnection,BrumeData,
+  tee, sender)(brumeParams);
---
+++ pipe.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// From "Mastering JavaScript Functional Programming", Federico Kereki
+const pipeKereki = (...fns) =>
+	fns.reduce((result, f) => 
+		(...args) => 
+			f(result(...args)));
+
+// Functionally the same as pipeKereki but more clear IMO
+const pipe = (f1, ...restFns) =>
+	(...args) => 
+		restFns.reduce((result, f) => 
+			f(result), f1(...args));
+
+// From "Mastering JavaScript Functional Programming", Federico Kereki
+const tap = fn => x => (fn(x), x)
+// Usage:
+// const tee = tap(fn)
+// pipe(..., tee, ...)(...args)
+
+module.exports = {pipe, tap}
