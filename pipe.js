f4e2ffc794747955e12e6249e1fb4bf3ef6cf000
Bob Lund <b.lund@cablelabs.com>
2022-03-28 14:20:31 -0600

    Added asyncPipe

--- pipe.js
+++ pipe.js
@@ -12,10 +12,19 @@ const pipe = (f1, ...restFns) =>
 		restFns.reduce((result, f) => 
 			f(result), f1(...args));
 
+const asyncPipe = (...fns) =>
+	async (args) => {
+    		let _args = args;
+			for(let f of fns) {
+       				_args = f.constructor.name == 'AsyncFunction' ? await f(_args) : f(_args);
+    	}
+    return _args;
+  }
+
 // From "Mastering JavaScript Functional Programming", Federico Kereki
 const tap = fn => x => (fn(x), x)
 // Usage:
 // const tee = tap(fn)
 // pipe(..., tee, ...)(...args)
 
-module.exports = {pipe, tap}
+module.exports = {pipe, asyncPipe, tap}
